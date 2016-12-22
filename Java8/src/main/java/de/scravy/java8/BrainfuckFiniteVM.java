c548120b6c3df9868df46258336000ffcdf23b47
Julian Fleischer <tirednesscankill@warhog.net>
2016-12-22 11:12:26 +0100

    Removed the incomplete Java implementation

--- Java8/src/main/java/de/scravy/java8/BrainfuckFiniteVM.java
+++
@@ -1,9 +0,0 @@
-package de.scravy.java8;
-
-public class BrainfuckFiniteVM implements BrainfuckVM {
-
-	@Override
-	public void run(final BrainfuckScript script) {
-	}
-
-}
--- Java8/src/main/java/de/scravy/java8/BrainfuckInfiniteVM.java
+++
@@ -1,9 +0,0 @@
-package de.scravy.java8;
-
-public class BrainfuckInfiniteVM implements BrainfuckVM {
-
-	public void run(BrainfuckScript script) {
-		
-	}
-	
-}
